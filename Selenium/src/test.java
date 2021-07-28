3d43a70ea6b6c57c8bd335054a0be4d1fdf239d3
Yi-00 <duccn22@gmail.com>
2021-07-28 20:28:11 +0700

    ok

--- Selenium/src/test.java
+++ Selenium/src/test.java
@@ -1,10 +1,10 @@
 public class test {
     public static void main(String[] args) {
-        int a = 10;
-        for(int i = 0; i < a; i++){
-            System.out.println(i);
-            break;
+        // Khai báo một mảng
+        int[] arr = {1, 2, 5, 8, 9};
+// Sử dụng vòng lặp for each để lặp qua mảng
+        for(int n : arr) {
+            System.out.println(n);
         }
-        System.out.println("xxxx");
     }
 }
