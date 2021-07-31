307d708c4e359ac2e2bebdfc48a0a56343e9d48e
Yi-00 <duccn22@gmail.com>
2021-07-31 07:54:18 +0700

    31/07

--- Selenium/src/test.java
+++ Selenium/src/test.java
@@ -1,29 +1,54 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
-    WebDriver driver;
-    String url;
+    private WebDriver driver;
+    private JavascriptExecutor js;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() throws Exception {
         driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+
+        // Maximize the browser's window
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
-    void test(){
-        //getHeight
-        double size = driver.manage().window().getSize().getHeight();
-        System.out.println(size);
+    public void testJavaScriptExecution() throws Exception {
+        // ´ò¿ªÓ¦ÓÃ
+        js.executeScript("window.location = 'https://www.migang.com'");
+        Thread.sleep(3000);
 
+        // ÏòÏÂ¹ö¶¯
+        js.executeScript("window.scrollBy(0,1900);");
+        Thread.sleep(2000);
 
+        // ÏòÉÏ¹ö¶¯
+        js.executeScript("window.scrollBy(0,-1900);");
+        Thread.sleep(2000);
+
+        // ¹ö¶¯µ½Òª²Ù×÷µÄÔªËØµÄÎ»ÖÃ
+        WebElement element = driver.findElement(By.xpath("//h2/a[contains(text(),'Ã×¸×¶¨ÆÚ')]"));
+        js.executeScript("arguments[0].scrollIntoView(true);",element);
+        Thread.sleep(2000);
+        js.executeScript("window.scrollBy(0,-190);");
+
+//		element.click();
     }
 
-
-    @AfterEach
-    void tearDown() {
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
