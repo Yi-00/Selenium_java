da237412e63c9f72cd7f34030651219208cc010e
Yi-00 <duccn22@gmail.com>
2021-08-08 15:04:27 +0700

    factory

--- pageObjectModel/src/main/java/pageClass/FindBy_demo.java
+++ pageObjectModel/src/main/java/pageClass/FindBy_demo.java
@@ -1,10 +1,16 @@
-package pageClass;//Tách riêng các mục findElement, test,....  Udemy: 91
+//Tách riêng các mục findElement, test,....  Udemy: 91
 //Giao diện Home page
 
+package pageClass;
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class FindBy_demo {
+    WebDriver driver;
+
     //dùng @FindBy : tìm username theo xpath
     @FindBy(xpath="//a[@id='txtUsername']")
     WebElement username;
@@ -16,6 +22,44 @@ public class FindBy_demo {
     //dùng @FindBy : tìm login button theo xpath
     @FindBy(xpath="//a[@id='btnLogin']")
     WebElement loginBtn;
+
+    //Constructoe have param
+    public FindBy_demo(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getUsername() {
+        return username;
+    }
+
+    public void setUsername(WebElement username) {
+        this.username = username;
+    }
+
+    public WebElement getPassword() {
+        return password;
+    }
+
+    public void setPassword(WebElement password) {
+        this.password = password;
+    }
+
+    public WebElement getLoginBtn() {
+        return loginBtn;
+    }
+
+    public void setLoginBtn(WebElement loginBtn) {
+        this.loginBtn = loginBtn;
+    }
 
     //enter username
     public void inputUsername(){
--- pageObjectModel/src/test/java/FrameworkTestCae.java
+++ pageObjectModel/src/test/java/FrameworkTestCae.java
@@ -8,28 +8,39 @@ import pageClass.FindBy_demo;
 
 import java.util.concurrent.TimeUnit;
 
-public class LoginPage_FindBy {
+public class FrameworkTestCae {
     private WebDriver driver;
     private String baseUrl;
+    FindBy_demo find;
 
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
         baseUrl="https://opensource-demo.orangehrmlive.com/";
 
+        find = new FindBy_demo(driver);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
-    void test(){
+    void test_valid_pw_user(){
         //Load url
         driver.get(baseUrl);
 
+        find.inputPassword();
+        find.inputPassword();
+        find.clickLoginBtn();
+    }
 
+    @Test
+    void test_invalid_username(){
+        find.setUsername("");
     }
 
     @AfterEach
     void tearDown() {
+        driver.quit();
     }
 }
