5f486ce70a67ebd5cd339a1c77a0b3d15a6a52f4
Yi-00 <duccn22@gmail.com>
2021-08-11 22:10:09 +0700

    Parallel

---
+++ TestNG_Tutorial/src/test/java/Parallel_Demo/Parallel1_TestNG.java
@@ -0,0 +1,35 @@
+package Parallel_Demo;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Udemy: 119: test song song
+ * Nội dung class Parallel1_TestNG và Parallel2_TestNG là như nhau
+ */
+public class Parallel1_TestNG {
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testMethod1() throws InterruptedException {
+        System.out.println("TestNG Parallel : method 1");
+        Thread.sleep(3000);
+        System.out.println("More Steps 1");
+    }
+
+
+    @Test
+    public void testMethod2() throws InterruptedException {
+        System.out.println("TestNG Parallel : method 2");
+        Thread.sleep(3000);
+        System.out.println("More Steps 2");
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+    }
+}
