41d950b469588d633eff30fc3e8252704edfe40a
Yi-00 <duccn22@gmail.com>
2021-08-12 15:19:34 +0700

    1208

--- TestNG_Tutorial/src/test/java/test.java
+++ TestNG_Tutorial/src/test/java/test.java
@@ -8,25 +8,4 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class test {
-    WebDriver driver;
-    String url;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        url = "https://opensource-demo.orangehrmlive.com/";
-        driver.get(url);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void loginTest(){
-        LoginPage.inputValue(driver, "//input[@id='txtUsername']", "Admin");
-        LoginPage.inputValue(driver, "//input[@id='txtPassword']", "admin123");
-        LoginPage.clickElement(driver, "//input[@id='btnLogin']");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-    }
 }
