6860ef7b6ce558b503980b29ad9bb6dc80b385bd
QUAN <duccn22@gmail.com>
2021-08-20 08:25:44 +0700

    Driver

---
+++ TestNG_Tutorial/src/test/java/Cucumber/LoginCucumberDemo.java
@@ -0,0 +1,21 @@
+package Cucumber;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class LoginCucumberDemo {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+
+
+    @AfterMethod
+    public void tearDown() {
+    }
+}
