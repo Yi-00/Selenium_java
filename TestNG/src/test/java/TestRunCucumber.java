9cd2285fbf405662f25f0e56953be6353961f035
QUAN <duccn22@gmail.com>
2021-08-23 21:24:28 +0700

    Cucumber run file

--- TestNG/src/test/java/TestRunCucumber.java
+++ TestNG/src/test/java/TestRunCucumber.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/Login.feature",
-        glue = {"stepdefinition"}
+        glue = {"stepdefination"}
 )
 public class TestRunCucumber {
 }
--- TestNG/src/test/java/stepdefination/LoginTest.java
+++ TestNG/src/test/java/stepdefination/LoginTest.java
@@ -11,40 +11,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
     WebDriver driver;
+    String url = "https://opensource-demo.orangehrmlive.com/";
 
-    @Given("User is on Login Page")
+    @Given("^User is on Login Page$")
     public void user_is_on_login_page() {
         driver = new ChromeDriver();
-        String url = "https://opensource-demo.orangehrmlive.com/";
-
         driver.manage().window().maximize();
         driver.get(url);
+
     }
-    @When("User enter valid username")
+    @When("^User enter valid username$")
     public void user_enter_valid_username() {
-//        inputValue(driver, "//input[@id='txtUsername']", "Admin");
         WebElement element = driver.findElement(By.xpath("//input[@id='txtUsername']"));
         element.clear();
         element.sendKeys("Admin");
     }
-    @When("User enter valid password")
+    @When("^User enter valid password$")
     public void user_enter_valid_password() {
-//        inputValue(driver, "//input[@id='txtPassword']", "admin123");
         WebElement element = driver.findElement(By.xpath("//input[@id='txtPassword']"));
         element.clear();
         element.sendKeys("admin123");
     }
-    @When("User click on Login button")
+    @When("^User click on Login button$")
     public void user_click_on_login_button() {
-//        clickOnBtn(driver, "//input[@id='btnLogin']");
         WebElement element = driver.findElement(By.xpath("//input[@id='btnLogin']"));
         element.click();
     }
-    @Then("User can visit home page")
+    @Then("$User can visit home page$")
     public void user_can_visit_home_page() {
-        //Nếu login thành công mới tìm thấy element bên dưới
-        WebElement element = driver.findElement(By.xpath("//a[@id='menu_admin_viewAdminModule']"));
-//        getElement(driver, "//a[@id='menu_admin_viewAdminModule']");
+        WebElement element = driver.findElement(By.xpath("//a[@id='menu_dashboard_index']"));
+        element.isDisplayed();
     }
-
 }
